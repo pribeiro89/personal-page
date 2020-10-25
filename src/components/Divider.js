2f05fbdd22e89de226583265e4b155e97f0a43ba
patricia.ribeiro <patricia.ribeiro@turbinekreuzberg.com>
2020-10-25 23:32:45 +0100

    Add Dividir component and Style header

---
+++ src/components/Divider.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const Divider = ({ light }) => {
+  const color = light ? 'white' : 'secondary';
+
+  return (
+    <div className={`
+      flex items-center justify-center w-full mx-0 mt-5 mb-6 
+    `}>
+      <div className={`rounded-2xl mr-4 h-1 w-24 bg-${color} border-${color}`}></div>
+      <div className={`text-3xl text-${color}`}>
+        <i className="fas fa-star"></i>
+      </div>
+      <div className={`rounded-2xl ml-4 h-1 w-24 bg-${color} border-${color}`}></div>
+    </div>
+  );
+};
+
+export default Divider;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import Divider from './Divider';
 import Navigation from './Navigation';
 
-const Header = () => {
-  return (
-    <header>
-      <Navigation />
-    </header>
-  );
-}
+const Header = () => (
+  <header className="block">
+    <Navigation />
+    <div className="pt-40 pb-24 bg-primary text-white text-center">
+      <div className="container mx-auto px-4 flex flex-col items-center">
+        <img
+          className="w-64 mb-12"
+          alt="avatar"
+          src='https://avataaars.io/?avatarStyle=Transparent&topType=LongHairBob&accessoriesType=Prescription02&hairColor=BrownDark&facialHairType=Blank&clotheType=ShirtScoopNeck&clotheColor=Heather&eyeType=Happy&eyebrowType=DefaultNatural&mouthType=Smile&skinColor=Pale'
+        />
+        <h1 className="font-headings font-bold text-4xl md:text-6xl leading-tight my-0 uppercase">
+          Hi there,<br />
+            I'm Patricia Ribeiro!
+          </h1>
+        <Divider light={true} />
+        <p className="text-xl md:text-2xl font-light my-0">Proud Geek & Software Developer</p>
+      </div>
+    </div>
+  </header>
+);
 
 export default Header;
