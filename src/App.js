498dc7d8506abb7225f8887a574753db4edeac15
patricia.ribeiro <patricia.ribeiro@turbinekreuzberg.com>
2020-10-26 22:59:47 +0100

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { HashRouter, Route } from "react-router-dom";
 import ContentSection from './components/ContentSection';
 import Copyright from './components/Copyright';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import Countdown from './projects/Countdown/Countdown';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <ContentSection light={true}>
-        Portfolio goes here
-      </ContentSection>
-      <Footer />
-      <Copyright />
-    </div>
+    <HashRouter basename='/'>
+      <Route exact path="/" component={Home} />
+      <Route path="/countdown" component={Countdown} />
+    </HashRouter>
   );
 }
+
+const Home = () => (
+  <div>
+    <Header />
+    <ContentSection light={true}>
+      Portfolio goes here
+        </ContentSection>
+    <Footer />
+    <Copyright />
+  </div>
+);
 
 export default App;
