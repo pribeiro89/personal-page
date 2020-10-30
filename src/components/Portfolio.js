6663b96d24426ffb76c280c609921eee0900e15a
patricia.ribeiro <patricia.ribeiro@turbinekreuzberg.com>
2020-10-30 01:55:30 +0100

    Add buttons to portfolio card

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -7,7 +7,9 @@ const projects = [
     img: countdownImg,
     name: "Countdown Timer",
     techs: "React • TailwindCss",
-    description: "A simple countdown timer utility."
+    description: "A simple countdown timer utility.",
+    urlDemo: '',
+    urlCode: ''
   },
 ];
 
@@ -22,19 +24,45 @@ const Portfolio = () => {
           ">
             <div className="
               absolute top-0 left-0 opacity-0 bg-primary 
-              h-full w-full p-6 sm:p-12 lg:p-8 xl:p-12 hover:opacity-100 
+              h-full w-full p-6 sm:p-8 xl:p-12 hover:opacity-100 
               transition-all duration-500 ease-in-out
-              text-center text-white
+              text-center text-white flex flex-col justify-between
             ">
-              <h5 className="text-lg uppercase mb-3 tracking-wider">
-                {project.name}
-              </h5>
-              <p className="text-sm text-secondary font-semibold mb-3">
-                {project.techs}
-              </p>
-              <p>
-                {project.description}
-              </p>
+              <div className="flex flex-col space-y-3">
+                <h5 className="text-lg uppercase tracking-wider">
+                  {project.name}
+                </h5>
+                <p className="text-sm text-secondary font-semibold">
+                  {project.techs}
+                </p>
+                <p>
+                  {project.description}
+                </p>
+              </div>
+              <div className="flex justify-around">
+                <a
+                  className="
+                    py-2 px-3 uppercase text-xs text-secondary hover:text-white
+                    border border-transparent hover:border-secondary rounded bg-gray-100 hover:bg-transparent
+                    transition-colors duration-300 ease-linear shadow
+                  "
+                  href={project.urlCode}
+                >
+                  <i class="fas fa-code mr-1"></i>
+                  View Code
+                </a>
+                <a
+                  className="
+                    py-2 px-3 uppercase text-xs text-white
+                    border border-transparent hover:border-secondary rounded bg-secondary hover:bg-transparent
+                    transition-colors duration-300 ease-linear shadow
+                  "
+                  href={project.urlDemo}
+                >
+                  <i class="far fa-eye mr-1"></i>
+                  View Demo
+                </a>
+              </div>
             </div>
             <img
               class="align-middle border-none h-auto max-w-full"
